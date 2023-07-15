8ea94c7bc8ed654c0c04c0a45a9e5ba6363becfc
IwamotoKakeru <kganpipi@gmail.com>
2023-07-15 18:07:50 +0900

    Remove unused code

--- Assets/Main/Script/BoardMovement.cs
+++ Assets/Main/Script/BoardMovement.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class BoardMovement : MonoBehaviour
 {
-
-    // [SerializeField] private float rotationSpeed = 120.0f;
     public float RotationSpeed { get; set; }
     void Start()
     {
