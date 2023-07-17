e2d7375af14af6c65abbdffa9d35caf4e705465a
IwamotoKakeru <kganpipi@gmail.com>
2023-07-17 15:10:25 +0900

    Edit comment
    Edit comment

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -4,16 +4,21 @@ import { render, useGlobals } from "@reactunity/renderer";
 import { Slider } from "@reactunity/material";
 
 function App() {
-  // ReactUnity 初期化
+  // useGlobalの設定
   const globals = useGlobals();
   const boardMovement = globals.Board.GetComponent("BoardMovement");
 
-  // 回転速度の設定
+  // 回転速度用stateの宣言
   const [rotationSpeed, SetRotationSpeed] = useState(
     boardMovement.RotationSpeed
   );
+
+  // UIでの変更を反映させる関数
   const handleRotationSpeed = (speedValue) => {
+    // Stateへ反映
     SetRotationSpeed(speedValue);
+
+    // C#側に反映
     boardMovement.RotationSpeed = speedValue;
   };
 
