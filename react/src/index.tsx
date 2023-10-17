056af06e5e45bc33ef15990d60edf53b638f9353
IwamotoKakeru <kganpipi@gmail.com>
2023-10-18 05:56:07 +0900

    Edit ui

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
-import "./index.scss";
 import { render, useGlobals } from "@reactunity/renderer";
 import { Slider } from "@reactunity/material";
 import ClearButton from "./ClearButton";
+import "./index.css";
 
 function App() {
   // useGlobalの設定
   const globals = useGlobals();
   const boardMovement = globals.Board.GetComponent("BoardMovement");
+
+  // UI用定数の宣言
+  const INCREMENT_NUM: number = 1;
+  const MAX_ROTATION_SPEED: number = 512;
+  const MIN_ROTATION_SPEED: number = 0;
 
   // 回転速度用stateの宣言
   const [rotationSpeed, SetRotationSpeed] = useState(
@@ -16,48 +21,39 @@ function App() {
 
   // UIでの変更を反映させる関数
   const handleRotationSpeed = (speedValue) => {
+    if (speedValue < MIN_ROTATION_SPEED) {
+      speedValue = MIN_ROTATION_SPEED;
+    } else if (MAX_ROTATION_SPEED < speedValue) {
+      speedValue = MAX_ROTATION_SPEED;
+    } else {
+      // 何もしない
+    }
+
     // Stateへ反映
     SetRotationSpeed(speedValue);
-
     // C#側に反映
     boardMovement.RotationSpeed = speedValue;
   };
 
-  // UI用定数の宣言
-  const INCREMENT_NUM: number = 10;
-  const MAX_ROTATION_SPEED: number = 512;
-  const MIN_ROTATION_SPEED: number = -MAX_ROTATION_SPEED;
-
   return (
-    <div>
-      <text>{rotationSpeed}</text>
-      <scroll>
+    <div className="all-contents">
+      <text className="text">{rotationSpeed.toString()}</text>
+      <div className="increment-buttons">
+        <button onClick={() => handleRotationSpeed(0.0)}>0</button>
         <button
-          className="plus"
           onClick={() => handleRotationSpeed(rotationSpeed + INCREMENT_NUM)}
         >
           +
         </button>
-        <button className="reset" onClick={() => handleRotationSpeed(0.0)}>
-          0
-        </button>
-        <button
-          className="minus"
-          onClick={() => handleRotationSpeed(rotationSpeed - INCREMENT_NUM)}
-        >
-          -
-        </button>
-        <Slider
-          className="slider"
-          direction="vertical"
-          step={INCREMENT_NUM}
-          min={MIN_ROTATION_SPEED}
-          max={MAX_ROTATION_SPEED}
-          value={rotationSpeed}
-          onChange={handleRotationSpeed}
-        />
-        <ClearButton />
-      </scroll>
+      </div>
+      <Slider
+        step={INCREMENT_NUM}
+        min={MIN_ROTATION_SPEED}
+        max={MAX_ROTATION_SPEED}
+        value={rotationSpeed}
+        onChange={handleRotationSpeed}
+      />
+      <ClearButton />
     </div>
   );
 }
